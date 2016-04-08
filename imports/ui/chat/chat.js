e470e885ab24276f1a17a8a8027da80fd5ce50bb
zahidul <islamz1214@gmail.com>
2016-04-07 21:57:34 -0400

    Basic chat functionality

--- imports/ui/chat/chat.js
+++ imports/ui/chat/chat.js
@@ -1,3 +1,31 @@
-import { Template } from 'meteor/templating';
-
 import './chat.html';
+
+
+Template.messages.helpers({
+        messages: function() {
+        return Messages.find({}, { sort: { time: -1}});
+        }
+    });
+
+Template.chatInput.events = {
+      'click #sendButton' : function(event) {
+
+       const name = document.getElementById('name');
+       const message = document.getElementById('message');
+
+       if(name.value == '') {
+         name.value = 'Anonymous';
+       }
+
+       if(message.value != '') {
+         Messages.insert({
+           name: name.value,
+           message: message.value,
+           time: Date.now(),
+         });
+       }
+
+       name.value = '';
+       message.value = '';
+     }
+}
