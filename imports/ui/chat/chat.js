56981a5000f2477a1a8b9d1e55d1da1d9b3611ef
zahidul <islamz1214@gmail.com>
2016-04-25 21:57:29 -0400

    add direct-message functionality

--- imports/ui/chat/chat.js
+++ imports/ui/chat/chat.js
@@ -2,6 +2,7 @@ import './chat.html';
 import './planet.html';
 import './message.html';
 import './messages.html';
+import './directMessage.html';
 
 
 
@@ -13,32 +14,38 @@ Template.chat.helpers({
     return Meteor.user().username;
   },
   users: function() {
-    return Meteor.users.find({});
+    return Meteor.users.find({_id:{$ne:Meteor.userId()}});
   },
   userCount: function() {;
-    return Meteor.users.find({}).count();
+    return Meteor.users.find({_id:{$ne:Meteor.userId()}}).count();
   },
   planets: function () {
     return Planets.find();
   },
   planetCount: function() {
     return Planets.find({}).count();
-  }
+  },
+
 });
 
 Template.messages.helpers({
   messages: function() {
-    const currentPlanet = Session.get('planet');
+    let currentPlanet;
+    if(Session.get('planet') == "") {
+      currentPlanet = Session.get('user');
+    } else {
+      currentPlanet = Session.get('planet');
+    }
+    console.log(Meteor.userId());
     return Messages.find({'planetRoom': currentPlanet});
   },
 });
 
 
-
-
 Template.planet.helpers({
 	active: function () {
 		if (Session.get('planet') === this.name) {
+      Session.set('user', "");
 			return "active";
 		} else {
 			return "";
@@ -46,6 +53,53 @@ Template.planet.helpers({
 	}
 });
 
+
+Template.directMessage.helpers({
+	active: function () {
+		if (Session.get('user') === dm_generater(Meteor.user().username + this.username)) {
+      Session.set('planet', "");
+			return "active";
+		} else {
+			return "";
+		}
+	}
+});
+
+
+
+function getRoom() {
+  if(Session.get('planet') == "") {
+    return Session.get('user');
+  } else {
+    return Session.get('planet');
+  }
+}
+
+function dm_generater(roomId) {
+  //let dm = Session.get('user');
+
+  let dm = roomId;
+
+  let total = 0;
+
+  for(let i = 0; i<dm.length; i++)
+  {
+    if(!isNaN(dm[i])) {
+      total = total + parseInt(dm[i]);
+    } else {
+      if(dm.charCodeAt(i) >= 65 && dm.charCodeAt(i) <= 90) {
+        // Uppercase ASCII
+        total = total + (dm.charCodeAt(i) - 64);
+      } else {
+        // Lowercase ASCII
+        total = total +  (dm.charCodeAt(i) - 96);
+      }
+    }
+  }
+
+  return total;
+
+}
 
 Template.chat.events = {
   'keypress input#message' : function(event) {
@@ -58,7 +112,7 @@ Template.chat.events = {
         user: Meteor.user().username,
         message: message,
         time: Date.now(),
-        planetRoom: Session.get('planet'),
+        planetRoom: getRoom(),
       });
 
       document.getElementById('message').value = '';
@@ -66,7 +120,10 @@ Template.chat.events = {
     }
   },
   'click .planet-list': function(event) {
-    console.log(this.name);
     Session.set('planet', this.name);
+  },
+  'click .directMessage-list': function(event) {
+    //Session.set('user', Meteor.user().username + this.username);
+    Session.set('user', dm_generater(Meteor.user().username + this.username));
   }
 }
