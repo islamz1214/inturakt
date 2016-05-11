c7df2e23e56519d13c423d9026b22b30e9d1417a
zahidul <islamz1214@gmail.com>
2016-05-11 07:09:58 -0400

    add https instead of http

--- imports/ui/chat/chat.js
+++ imports/ui/chat/chat.js
@@ -211,7 +211,8 @@ function giphyPicker(message, callback) {
 	q = message;
 
 	request = new XMLHttpRequest;
-	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true);
+  //"Powered by GIPHY"
+	request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true);
 
 	request.onload = function() {
 
